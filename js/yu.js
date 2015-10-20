9a16b12514f0d451f8803218d319ce9dc81e25cb
break <798332724@qq.com>
2015-10-20 15:45:13 +0800

    yujs

--- js/yu.js
+++ js/yu.js
@@ -1,8 +1,4 @@
-/*$(function(){
-	var oldwidth=$(".login").outerWidth();
-	$(".login").css("margin-left",-oldwidth/2+"px");
-	$(window).resize(function(){
-		var width=$(".login").outerWidth();
-		$(".login").css("margin-left",-width/2+"px");
-	})
-})*/
+
+$(function(){
+	$(".loading").fadeOut();
+})
