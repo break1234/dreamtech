a2e07caa82d4a6fe730ca19a97a9c22a3f731755
break <798332724@qq.com>
2015-10-20 14:18:41 +0800

    dreamtech

---
+++ js/yu.js
@@ -0,0 +1,8 @@
+/*$(function(){
+	var oldwidth=$(".login").outerWidth();
+	$(".login").css("margin-left",-oldwidth/2+"px");
+	$(window).resize(function(){
+		var width=$(".login").outerWidth();
+		$(".login").css("margin-left",-width/2+"px");
+	})
+})*/
