b27e9a1d852fba2135fcf2196205b6ff89f7a61a
break <798332724@qq.com>
2015-10-21 13:39:30 +0800

    js

--- js/yu.js
+++ js/yu.js
@@ -1,4 +1,7 @@
 
 $(function(){
 	$(".lager-header").css("height",document.documentElement.scrollHeight+"px");
+	$(".dropdown-menu li").click(function(){
+		$(".input-group input").val($(this).text());
+	})
 })
