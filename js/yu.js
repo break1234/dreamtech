22c74fbe722afa255526189a0652deef1515ace7
break <798332724@qq.com>
2015-10-21 14:50:40 +0800

    js

--- js/yu.js
+++ js/yu.js
@@ -1,18 +1,4 @@
 
 $(function(){
 	$(".lager-header").css("height",document.documentElement.scrollHeight+"px");
-	$(".dropdown-menu li").click(function(){
-		$(".input-group input").val($(this).text());
-	})
-	$(".input-group-btn").click(function(){
-		//$(".dropdown-menu").slideDown();
-		alert("click")
-	})
-	$("input").focus(function(){
-		$(".dropdown-menu").slideUp();
-		alert("1")
-	})
-	$("li").click(function(){
-		$(".dropdown-menu").slideUp();
-	})
 })
