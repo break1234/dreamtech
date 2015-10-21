607a51382dd1ed64b84d623f04441909739ae6c9
break <798332724@qq.com>
2015-10-21 14:06:42 +0800

    slide

--- js/yu.js
+++ js/yu.js
@@ -4,4 +4,13 @@ $(function(){
 	$(".dropdown-menu li").click(function(){
 		$(".input-group input").val($(this).text());
 	})
+	$(".input-group-btn").click(function(){
+		$(".dropdown-menu").slideDown();
+	})
+	$("input").focus(function(){
+		$(".dropdown-menu").slideUp();
+	})
+	$("li").click(function(){
+		$(".dropdown-menu").slideUp();
+	})
 })
